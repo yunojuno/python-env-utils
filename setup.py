b37786dad37aee3758c886f3dd17ab262a700d53
Hugo Rodger-Brown <hugo@yunojuno.com>
2016-11-24 10:23:01 +0000

    Bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
 """Setup file for env_utils."""
 import os
-from os.path import join, dirname, normpath, abspath
+from os.path import join, normpath, abspath
 from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+# requirements.txt must be included in MANIFEST.in and include_package_data must be True
+# in order for this to work; ensures that tox can use the setup to enforce requirements
+REQUIREMENTS = '\n'.join(open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).readlines())  # noqa
 
 # allow setup.py to be run from any path
 os.chdir(normpath(join(abspath(__file__), os.pardir)))
 
 setup(
     name="env_utils",
-    version="1.1.0",
+    version="1.2.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['python-dateutil>=2.6'],
-    license=open(join(dirname(__file__), 'LICENCE.md')).read(),
+    install_requires=REQUIREMENTS,
+    license='MIT',
     description="Utility functions to make it easier to work with os.environ",
-    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    long_description=README,
     url='https://github.com/yunojuno/python-env-utils',
     author="Hugo Rodger-Brown",
     author_email='hugo@yunojuno.com',
